394494ca8d7c652d4722fdcfcfb8d6a6c2d2375c
Yaqub <murcja812@gmail.com>
2020-04-13 19:40:14 +0200

    before

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MapScreen } from './src/screens/MapScreen'
 import {
   View, StyleSheet, Text, NativeEventEmitter,
-  EmitterSubscription, Linking, Button
+  EmitterSubscription, Linking, Button, TextInput
 } from 'react-native'
 import { BottomBar } from './src/components/BottomBar'
 import { Tab } from './src/interfaces/tab'
@@ -16,31 +16,34 @@ interface IProps { }
 interface IState { activeTab: Tab, status?: string }
 
 export default class App extends React.Component<IProps, IState> {
-  eventListener: EmitterSubscription
+  spotifySdk: EmitterSubscription
+  loginFields = {
+    email: '',
+    password: ''
+  }
+  btnRef: any
   constructor(props: IProps) {
     super(props)
     this.changeTab = this.changeTab.bind(this)
     this.InnerScreen = this.InnerScreen.bind(this)
+    this.FirebaseLogin = this.FirebaseLogin.bind(this)
     this.state = {
       activeTab: 'explore'
     }
   }
 
   componentDidMount() {
-    // this.getUrlAsync().then(console.log)
-    // Linking.addEventListener('url', console.log)
     const eventEmitter = new NativeEventEmitter(SpotifyModule)
     // this.eventListener = eventEmitter.addListener('playerStateChanged', event => {
     //   console.log('playerStateChanged', event)
     // })
-    this.eventListener = eventEmitter.addListener('status', event => {
-      console.log('statusEvent', event)
-      this.setState({ status: event })
+    this.spotifySdk = eventEmitter.addListener('status', async event => {
+      this.setState({ status: 'disconnected' })
     })
   }
 
   componentWillUnmount() {
-    if (this.eventListener) this.eventListener.remove()
+    if (this.spotifySdk) this.spotifySdk.remove()
   }
 
   render() {
@@ -48,6 +51,29 @@ export default class App extends React.Component<IProps, IState> {
       <View style={styles.container}>
         {this.state.status == 'connected' ? <this.InnerScreen /> : null}
         {this.state.status == 'disconnected' ? <LoginScreen /> : null}
+        {this.state.status == 'logging' ? <this.FirebaseLogin /> : null}
+      </View>
+    )
+  }
+
+  async login() {
+    const loginRef = await backendService.user.
+      signInWithEmailAndPassword(this.loginFields.email, this.loginFields.password)
+    this.setState({ status: 'connected' })
+  }
+
+  FirebaseLogin() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.chatTextInput}
+          placeholder='Email'
+          onChangeText={text => this.loginFields.email = text} />
+        <TextInput
+          style={styles.chatTextInput}
+          placeholder='Password'
+          onChangeText={text => this.loginFields.password = text} />
+        <Button onPress={() => this.login()} title={'Login'} ref={this.btnRef} />
       </View>
     )
   }
@@ -82,5 +108,8 @@ const styles = StyleSheet.create({
   },
   appTitle: {
     fontSize: 40, color: 'white', fontFamily: 'MavenProBold', marginTop: 45
+  },
+  chatTextInput: {
+    width: 210
   }
 })
